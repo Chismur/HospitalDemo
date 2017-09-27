56af9ae36dfa824ae0816fdebb81337a3d0a7511
Matvey Malik <matikstyle@gmail.com>
2017-09-27 17:29:35 +0300

    добавил свой стиль my.css, подключил в fxml, для button

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root,300,400);
+        //scene.getStylesheets().add(getClass().getResource("/styles/my.css").toExternalForm());
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
